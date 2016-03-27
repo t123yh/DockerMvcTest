385311795a28cb8ae2e12f8d2b90f5538577e8e7
t123yh <t123yh@outlook.com>
2016-03-27 10:45:14 +0800

    Add npm, bpwer and gulp support.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+var bower = require('gulp-bower');
+
+gulp.task('bower', function () {
+    return bower()
+           .pipe(gulp.dest('wwwroot/lib/'))
+});
